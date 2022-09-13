91a1d845a61d95cea13868c48c9c369641656537
ParamonenkoOksana <oksana1paramonenko@gmail.com>
2022-09-13 08:03:24 +0300

    ДЗ/8

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu'),
-    // body: document.querySelector('[data-modal]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
     // refs.body.classList.toggle('no-scroll');
-
   }
 })();
+
